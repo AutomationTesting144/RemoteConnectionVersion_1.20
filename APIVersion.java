97a1888809a32c126ab192716b8b0be19c2dc611
AutomationTesting144 <automationtesting144@gmail.com>
2017-07-21 11:06:15 -0700

    Version 1.20 Remote Connection

---
+++ APIVersion.java
@@ -0,0 +1,48 @@
+package com.example.a310287808.ifttt_remoteconnection_120;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by 310287808 on 7/12/2017.
+ */
+
+public class APIVersion {
+    public String IPAddress = "192.168.86.23/api";
+    public String HueUserName = "yVqFoICUnIFDpxpGUCmXnHtAnv2sd1DJJ5LLHcGo";
+    public String HueBridgeParameterType = "config";
+    //public String HueBridgeIndLightType = "lights";
+    public String finalURL;
+    public String apiversion;
+
+
+    public String getAPIVersion() throws IOException, JSONException {
+
+        finalURL= "http://" + IPAddress + "/" + HueUserName + "/" + HueBridgeParameterType;
+        URL url1 = new URL(finalURL);
+        HttpURLConnection connection;
+        connection = (HttpURLConnection) url1.openConnection();
+        connection.connect();
+        InputStream stream1 = connection.getInputStream();
+        BufferedReader reader1 = new BufferedReader(new InputStreamReader(stream1));
+        StringBuffer br1 = new StringBuffer();
+        String line1 = " ";
+        String change = null;
+        while ((line1 = reader1.readLine()) != null) {
+            //change = line1.replace("[", "").replace("]", "");
+            br1.append(line1);
+        }
+        String output1 = br1.toString();
+        JSONObject apiObject = new JSONObject(output1);
+        Object apiObjectValue = apiObject.get("apiversion");
+        apiversion = apiObjectValue.toString();
+        return apiversion;
+    }
+}
---
+++ SWVersion.java
@@ -0,0 +1,51 @@
+package com.example.a310287808.ifttt_remoteconnection_120;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by 310287808 on 7/12/2017.
+ */
+
+public class SWVersion {
+    public String IPAddress = "192.168.86.23/api";
+    public String HueUserName = "yVqFoICUnIFDpxpGUCmXnHtAnv2sd1DJJ5LLHcGo";
+    public String HueBridgeParameterType = "config";
+    //public String HueBridgeIndLightType = "lights";
+    public String finalURL;
+    public String SWversion;
+
+    public String getSWVersion() throws IOException, JSONException {
+        finalURL= "http://" + IPAddress + "/" + HueUserName + "/" + HueBridgeParameterType;
+        URL url1 = new URL(finalURL);
+        HttpURLConnection connection;
+        connection = (HttpURLConnection) url1.openConnection();
+        connection.connect();
+
+        InputStream stream1 = connection.getInputStream();
+
+        BufferedReader reader1 = new BufferedReader(new InputStreamReader(stream1));
+
+        StringBuffer br1 = new StringBuffer();
+
+        String line1 = " ";
+        String change = null;
+        while ((line1 = reader1.readLine()) != null) {
+            //change = line1.replace("[", "").replace("]", "");
+            br1.append(line1);
+        }
+        String output1 = br1.toString();
+
+        JSONObject SWObject = new JSONObject(output1);
+        Object SWObjectValue = SWObject.get("swversion");
+        SWversion = SWObjectValue.toString();
+        return SWversion;
+    }
+}
